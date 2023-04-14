e4cb393cec3ffa48b9a877a8551749b50d81b6cf
阿博-斯 <aolong0714@qq.com>
2023-04-15 02:09:57 +0800

    v0.6

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import math
 
 
 class Pagerank:
-    def __init__(self, iter_num, block_size):
-        self.p = 0.85
-        self.iter_num = iter_num
+    def __init__(self, max_iter=100, block_size=1000, alpha=0.85):
+        self.p = alpha
+        self.max_iter = max_iter
         self.block_size = block_size
 
         self.graph = Graph()
@@ -38,21 +38,21 @@ class Pagerank:
         return
 
     def cal_PR(self):
-        for i in range(self.iter_num):
+        for i in range(self.max_iter):
             self.PR_iter()
             print("iter %3d finish" % i)
         self.out_res()
 
     def block_cal_PR(self):
-        for i in range(self.iter_num):
+        for i in range(self.max_iter):
             block_PR = None
-            for j in range(math.ceil(self.N/self.block_size)):
-                start, end = j*self.block_size, min(self.N, (j+1)*self.block_size)
-                g = np.zeros((end-start, self.N), dtype=np.float64)
+            for j in range(math.ceil(self.N / self.block_size)):
+                start, end = j * self.block_size, min(self.N, (j + 1) * self.block_size)
+                g = np.zeros((end - start, self.N), dtype=np.float64)
                 for num in range(start, end):
                     for node in self.in_degree[num]:
                         idx_node = self.nodes.index(node)
-                        g[num-start, idx_node] = 1/self.V[idx_node]
+                        g[num - start, idx_node] = 1 / self.V[idx_node]
                 if block_PR is not None:
                     block_PR = np.concatenate([block_PR, self.block_process(self.PR, g, self.p)], axis=0)
                 else:
@@ -64,16 +64,20 @@ class Pagerank:
     def out_res(self):
         res = heapq.nlargest(100, enumerate(self.PR), key=lambda x: x[1])
         top_id = []
-        for idx, pr in res:
-            top_id.append(self.nodes[idx])
-        print(top_id)
+        for i, (idx, pr) in enumerate(res):
+            print("Top %3d: %4d %6f" % (i, self.nodes[idx], pr))
+            top_id.append((self.nodes[idx], pr))
+        with open('result.txt', 'w') as f:
+            for item in top_id:
+                f.write("%d %f\n" % (item[0], item[1]))
+
     @staticmethod
     def block_process(PR, graph, p):
-        res = p*np.dot(graph, PR)
-        return np.ones(res.size) * (1-p)/PR.size + res
+        res = p * np.dot(graph, PR)
+        return np.ones(res.size) * (1 - p) / PR.size + res
 
 
 if __name__ == '__main__':
-    p = Pagerank(100, 1000)
+    p = Pagerank()
     p.block_cal_PR()
     # p.cal_PR()
